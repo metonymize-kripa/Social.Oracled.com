ff42775f6ca3a5fc6b7c0e4c1792d4b240e0ccd0
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-21 16:37:33 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -175,9 +175,8 @@ font-size:4rem !important;
 <div class="row card">
 	<table>
 		<tr>
-			<td width="100%"><p align="center"><iframe width="80%" height=420 src="https://public.com/stocks/{ticker}/embed" frameborder="0" allow="encrypted-media" allowfullscreen allowtransparency></iframe>
-			</p> 
-			</td>
+		<p align="center"><iframe width="80%" height=420 src="https://public.com/stocks/{ticker}/embed" frameborder="0" allow="encrypted-media" allowfullscreen allowtransparency></iframe>
+		</p> 
 		</tr>
 		<tr>
 		    <td width="20%"><a style="color:#168ed7;font-size:2rem;" href="https://twitter.com/share?url={post_url}{ticker}&text={post_title}{ticker}&hashtags={ticker}" class="button fa fa-twitter pull-left"></a></td>
