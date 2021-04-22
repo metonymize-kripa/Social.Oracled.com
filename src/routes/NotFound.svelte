75afcda319c59c4b13b6248c21be3d50d683d687
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-22 13:17:04 -0500

    testing push routing

--- src/routes/NotFound.svelte
+++ src/routes/NotFound.svelte
@@ -1,3 +1,9 @@
+<script>
+
+import {push, pop, replace} from 'svelte-spa-router'
+  
+</script>
+
 <h2>NotFound</h2>
 
-<p>Oops, this route doesn't exist!</p>
+<button on:click={() => push('/stock/spy')}>Click to start with SPY</button>
