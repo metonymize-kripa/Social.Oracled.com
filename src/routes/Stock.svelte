6971b7ce15c04ed970b41bc11556c2df5b4c2795
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-22 11:27:58 -0500

    test file

---
+++ src/routes/Stock.svelte
@@ -0,0 +1,12 @@
+<h2>Hi there!</h2>
+
+<p>
+    Your route is:
+    <b>{params.symbol}</b>
+    <b>{#if params.cmd}{params.cmd}{/if}</b>
+</p>
+
+<script>
+// The params prop contains values matched from the URL
+export let params = {}
+</script>
