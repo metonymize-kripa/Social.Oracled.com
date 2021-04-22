02fbb1262ee2c6aa4866f243072dfd3ae6ea8843
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-22 11:31:44 -0500

    added home

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 // Components
+import Home from './routes/Home.svelte'
 import Stock from './routes/Stock.svelte'
 import NotFound from './routes/NotFound.svelte'
 
 // Export the route definition object
 export default {
+    // Exact path
+    '/': Home,
 
     // Using named parameters, with last being optional
     '/stock/:symbol/:cmd?': Stock,
