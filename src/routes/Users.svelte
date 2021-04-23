68fa4ccef045ca4ce326f68296c9cfeaa3166fc2
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-23 15:17:26 -0500

    testing avataar generation

--- src/routes/Users.svelte
+++ src/routes/Users.svelte
@@ -6,8 +6,152 @@
 
 <script>
   import { onMount } from "svelte";
+  import _ from "underscore";
   import Avatar from "svelte-avatar";
   import {push, pop, replace} from 'svelte-spa-router'
+
+ const avataar_inputs = {
+    "avatar-style": [
+        "Circle",
+        "Transparent"
+    ],
+    "topType" : [
+        "NoHair",
+        "Eyepatch",
+        "Hat",
+        "Hijab",
+        "Turban",
+        "WinterHat1",
+        "WinterHat2",
+        "WinterHat3",
+        "WinterHat4",
+        "LongHairBigHair",
+        "LongHairBob",
+        "LongHairBun",
+        "LongHairCurly",
+        "LongHairCurvy",
+        "LongHairDreads",
+        "LongHairFrida",
+        "LongHairFro",
+        "LongHairFroBand",
+        "LongHairNotTooLong",
+        "LongHairShavedSides",
+        "LongHairMiaWallace",
+        "LongHairStraight",
+        "LongHairStraight2",
+        "LongHairStraightStrand",
+        "ShortHairDreads01",
+        "ShortHairDreads02",
+        "ShortHairFrizzle",
+        "ShortHairShaggyMullet",
+        "ShortHairShortCurly",
+        "ShortHairShortFlat",
+        "ShortHairShortRound",
+        "ShortHairShortWaved",
+        "ShortHairSides",
+        "ShortHairTheCaesar",
+        "ShortHairTheCaesarSidePart"
+    ],
+    "accessoriesType": [
+        "Blank",
+        "Kurt",
+        "Prescription01",
+        "Prescription02",
+        "Round",
+        "Sunglasses",
+        "Wayfarers"
+    ],
+
+    "hairColor": [
+        "Auburn",
+        "Black",
+        "Blonde",
+        "BlondeGolden",
+        "Brown",
+        "BrownDark",
+        "PastelPink",
+        "Platinum",
+        "Red",
+        "SilverGray"
+    ],
+    "facialHairType": [
+        "Blank",
+        "BeardMedium",
+        "BeardLight",
+        "BeardMagestic",
+        "MoustacheFancy",
+        "MoustacheMagnum"
+    ],
+    "clotheType": [
+        "BlazerShirt",
+        "BlazerSweater",
+        "CollarSweater",
+        "GraphicShirt",
+        "Hoodie",
+        "Overall",
+        "ShirtCrewNeck",
+        "ShirtScoopNeck",
+        "ShirtVNeck"
+    ],
+    "eyeType": [
+        "Close",
+        "Cry",
+        "Default",
+        "Dizzy",
+        "EyeRoll",
+        "Happy",
+        "Hearts",
+        "Side",
+        "Squint",
+        "Surprised",
+        "Wink",
+        "WinkWacky"
+    ],
+    "eyebrowType": [
+        "Angry",
+        "AngryNatural",
+        "Default",
+        "DefaultNatural",
+        "FlatNatural",
+        "RaisedExcited",
+        "RaisedExcitedNatural",
+        "SadConcerned",
+        "SadConcernedNatural",
+        "UnibrowNatural",
+        "UpDown",
+        "UpDownNatural"
+    ],
+    "mouthType": [
+        "Concerned",
+        "Default",
+        "Disbelief",
+        "Eating",
+        "Grimace",
+        "Sad",
+        "ScreamOpen",
+        "Serious",
+        "Smile",
+        "Tongue",
+        "Twinkle",
+        "Vomit"
+    ],
+    "skinColor": [
+        "Tanned",
+        "Yellow",
+        "Pale",
+        "Light",
+        "Brown",
+        "DarkBrown",
+        "Black"
+    ]
+}
+ 
+ function createRandomAvataar(){
+	 return 'https://avataaars.io/?'+
+		 'avatarStyle=Circle&'+
+		 'topType=ShortHairShortCurly&'+
+	 'accessoriesType=Prescription01&hairColor=BlondeGolden&facialHairType=Blank&clotheType=BlazerSweater&eyeType=Surprised&eyebrowType=RaisedExcited&mouthType=Eating&skinColor=Pale';
+ }
 	
   let user_array = ['FatTony','Pappe','Kripa','Pani', 'Harsha','Brad','Sunil','Deba']
   let new_user= 'FatTony';
@@ -44,6 +188,7 @@ function changeUser(new_user){
         <div class="col-8 " >
             <h3>Check Favorites</h3>
             {#each user_array as tx}
+		{createRandomAvataar}
                 <button class="button"  style="font:1rem;padding:0.8rem; margin:0.2rem; background:#351eb5;color:white;" on:click={() => changeUser(tx)}>{tx}</button>
             {/each}
         </div>
