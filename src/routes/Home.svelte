347244a4a6867e51065dd4a0a2023bf666f45d53
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-25 11:25:03 -0500

    pulling component back

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -8,7 +8,21 @@
   import { onMount } from "svelte";
   import Avatar from "svelte-avatar";
   import RangeSlider from "svelte-range-slider-pips";
-  import Auth from "./utils/Auth.svelte"
+  //import Auth from "./utils/Auth.svelte"
+  import {
+	  Auth0Context,
+	  Auth0LoginButton,
+	  Auth0LogoutButton,
+	  authError,
+	  authToken,
+	  idToken,
+	  isAuthenticated,
+	  isLoading,
+	  login,
+	  logout,
+	  userInfo,
+	} from '@dopry/svelte-auth0';
+	
   let api_output ={};
   let friend_output={};
 
@@ -185,7 +199,24 @@ let post_title =  encodeURIComponent("Here's the upshot for ");
 
 <body>
 
-<Auth />
+<!-- <Auth />
+-->
+<Auth0Context domain="dev-gh9on756.us.auth0.com" client_id="lDh9u5tdu1Kk5CkXtZjmjjmUKuGARk0v">
+    <div class="row">
+	    {#if !$isAuthenticated}
+		<div class="col-9"><h1>💎Oracle Upshot</h1></div>
+		<div class="col-3">
+		    <Auth0LoginButton class="button text-center error is-full-width is-big" >Login</Auth0LoginButton>
+		</div>
+	    {:else}
+		    <div class="col-9"><h1>💎Oracle Upshot</h1></div>
+		    <div class="col-3 hide-xs">
+			 <span class="tag is-large">Welcome {$userInfo["nickname"]}</span>
+		     </div>
+			{getMyRating($userInfo["email"]) || ""}
+	    {/if}
+    </div>
+  </Auth0Context>
 
 {#if show_entry_card}
     <div class="card" >
