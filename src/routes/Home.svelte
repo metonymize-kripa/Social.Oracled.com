60e1ca6b892c5445a9cea3038a7dcc6cca8d2f84
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-28 09:13:36 -0500

    Update Home.svelte

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-  <title>Social traders, Level up -- Home</title>
+  <title>Enter 💎Oracle</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://unpkg.com/chota@latest">
 </svelte:head>
