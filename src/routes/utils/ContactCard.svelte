c378e51f4d994816bf51e760fd74d7ac63c695ca
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-29 11:07:27 -0500

    Update ContactCard.svelte

--- src/routes/utils/ContactCard.svelte
+++ src/routes/utils/ContactCard.svelte
@@ -32,14 +32,12 @@ else {
 
 	h1 {
 		border-bottom: 1px solid #aaa;
-		margin: 0 0 1em 0;
 		font-size: 50px;
 		color: #7d03fc;
 	}
 	
 	h2 {
 		text-align: center;
-		margin: 0 0 0 0;
 		font-size: 20px;
 		color: #aaa;
 	}
