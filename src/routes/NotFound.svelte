e4f35a487afb573fe74a5b3319b7d606f26c17ec
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-28 19:22:28 -0500

    updated with contact card

--- src/routes/NotFound.svelte
+++ src/routes/NotFound.svelte
@@ -1,9 +1,24 @@
 <script>
 
 import {push, pop, replace} from 'svelte-spa-router'
+import ContactCard from './utils/ContactCard.svelte';
   
 </script>
 
 <h2>NotFound</h2>
 
 <button on:click={() => push('/stock/spy')}>Click to start with SPY</button>
+
+<ContactCard>
+	<span slot="name">
+		Bearfox
+	</span>
+
+	<span slot="address">
+		My sincere apologies
+	</span>
+	
+	<span slot="email">
+		upshot@oracled.com
+	</span>
+</ContactCard>
