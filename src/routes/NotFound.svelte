d384b2cd553de67177bfffa8c49865bd16f2535c
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-29 11:26:08 -0500

    Update NotFound.svelte

--- src/routes/NotFound.svelte
+++ src/routes/NotFound.svelte
@@ -1,11 +1,23 @@
-<script>
+<svelte:head>
+	<title>Page Not Found</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+</svelte:head>
 
+<script>
 import {push, pop, replace} from 'svelte-spa-router'
 //import ContactCard from './utils/UserCard.svelte';
 import ContactCard from './utils/ContactCard.svelte';
 import TickerCard from './utils/TickerCard.svelte';
-  
 </script>
+
+<style>
+	body { 
+	font-family: 'Roboto', sans-serif;
+	}
+</style>
+
+<body>
 
 <h2>NotFound</h2>
 
@@ -16,6 +28,7 @@ import TickerCard from './utils/TickerCard.svelte';
 <ContactCard my_name="Harmeet" my_rating=55 my_freshness="56%" my_accuracy="63%"/>
 <TickerCard ticker="IBM" rating=56 accuracy=80 freshness=1256/>
 
+</body>
 
 <!--
 <ContactCard>
