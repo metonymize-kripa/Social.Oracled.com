12769ed477784af09c140e443494f2e9919a9543
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-29 09:43:04 -0500

    Update NotFound.svelte

--- src/routes/NotFound.svelte
+++ src/routes/NotFound.svelte
@@ -1,7 +1,8 @@
 <script>
 
 import {push, pop, replace} from 'svelte-spa-router'
-import ContactCard from './utils/UserCard.svelte';
+//import ContactCard from './utils/UserCard.svelte';
+import ContactCard from './utils/ContactCard.svelte';
 import TickerCard from './utils/TickerCard.svelte';
   
 </script>
@@ -12,7 +13,7 @@ import TickerCard from './utils/TickerCard.svelte';
 
 <br>
 
-<ContactCard my_name="Harmeet" my_status="SuperMan" accuracy=80 followers=1256/>
+<ContactCard my_name="Harmeet" my_freshness="56%" my_accuracy="63%"/>
 <TickerCard ticker="IBM" rating=56 accuracy=80 freshness=1256/>
 
 
